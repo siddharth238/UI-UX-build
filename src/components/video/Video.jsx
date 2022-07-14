3223133d0ea129d1751082203996a6d9792dfe57
siddharth238 <62851444+siddharth238@users.noreply.github.com>
2022-07-14 18:18:28 +0530

    Update Video.jsx

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -6,13 +6,13 @@ import { motion } from 'framer-motion';
 function Video({Src, left, title, text,link}) {
   return (
  
-    <div className={left?"gpt3__possibility section__padding left":"gpt3__possibility section__padding"} id="possibility">
+    <div className={left?"Video section__padding left":"gpt3__possibility section__padding"} id="possibility">
     <motion.div  whileInView={{ x: [left?70: -70, 0], opacity: [0, 1] }}
-        transition={{ type: "spring", stiffness: 100   }} className="gpt3__possibility-image oveflowh">
+        transition={{ type: "spring", stiffness: 100   }} className="Video-image oveflowh">
     <ReactPlayer className='img' controls url={Src}/ >
     </motion.div>
     <motion.div   whileInView={{ x: [left?-70: 70, 0], opacity: [0, 1] }}
-        transition={{ type: "spring", stiffness: 100   }} className="gpt3__possibility-content oveflowh">
+        transition={{ type: "spring", stiffness: 100   }} className="Video-content oveflowh">
       <h4><a target="_blank" href='https://srmap.edu.in'>• View site</a></h4>
       <h1 className="gradient__text">{title}</h1>
       <p>{text}</p>
